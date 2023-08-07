2205442c4175ff5695921d77d17a1c8393a79ff9
manasa043 <137529300+manasa043@users.noreply.github.com>
2023-08-07 19:03:34 +0530

    Add files via upload
    program to print patterns

---
+++ pattern4.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print4(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<2*i+1;k++)
+    {
+        cout<<"*";
+    }
+    for(int l=0;l<n-i-1;l++)
+    {
+        cout<<" ";
+    }
+    cout<<endl;
+        
+    }
+    
+}
+int main()
+{
+    cout<<"enter the no.of rows to print"<<endl;
+    int n;
+    cin>>n;
+    print4(n);
+}
