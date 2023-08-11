296156b51b3b20c9f41c78079508518b2cfbe36d
manasa043 <137529300+manasa043@users.noreply.github.com>
2023-08-11 21:58:51 +0530

    Add files via upload
    pattern

---
+++ pattern8.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern8(int n)
+{
+    int start=1;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            start=1;
+    
+        }
+        else 
+        {
+        start=0;
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout<<start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern8(n);
+}
